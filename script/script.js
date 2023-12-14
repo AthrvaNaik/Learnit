fdb0bf8d065662fb4df701848aa905e9dca679ea
AthrvaNaik <141731433+AthrvaNaik@users.noreply.github.com>
2023-12-14 19:27:16 +0530

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,28 @@
+//changing navbar styles
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+})
+
+const faqs=document.querySelectorAll('.faq')
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open')
+
+
+        const icon=faq.querySelector('.faq_icon i')
+        if(icon.className==='fa-solid fa-plus'){
+            icon.className='fa-solid fa-minus'
+        }
+        else{
+            icon.className='fa-solid fa-plus'
+        }
+    })
+})
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
